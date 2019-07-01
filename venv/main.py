86be3130773ca4916dc2bf803bc9c30b7f3809a1
Denton Wood <Denton_Wood@baylor.edu>
2019-07-01 15:46:27 +0200

    Added PyTest framework

--- venv/main.py
+++ venv/main.py
@@ -1,15 +1,20 @@
+import collections
 from sources.filter import *
+from sources.Levenshtein import *
 
 def main():
     # Pull 2 repos from GitHub
     print("Pulling repositories")
     # Tokenize repos
     print("Tokenizing repositories")
+    list1 = {}
+    list2 = {}
     # Filter out variable names - only control structures
     print("Filtering repositories")
-    filter_tokens({}, {})
+    filter_tokens(list1, list2)
     # Use Levenhstein to compare tokens to tokens
     print("Comparing repositories")
+    lev_distance(list1, list2)
 
     print("Results:")
 
