72f1cf4efef58e2d428abcae732e85215047557a
calebbaylor <caleb_dehaan@baylor.edu>
2019-07-01 15:36:58 +0200

    small updates

--- venv/main.py
+++ venv/main.py
@@ -10,27 +10,29 @@ def main():
     if len(sys.argv) != 5 and len(sys.argv) != 3:
         print("""\
 Usage:
-    python3 main [git repo 1] [directory 1] [git repo 2] [directory 2]
+    python3 main.py [directory to be cloned to 1] [git repo 1] [directory to be cloned to 2] [git repo 2]
 or (for comparing within a directory):
-    python3 main [git repo 1] [directory 1]
+    python3 main.py [directory to be cloned to 1] [git repo 1]
         """)
         return
     elif len(sys.argv) == 3:
-        git_repo_1 = sys.argv[1]
-        cloned_directory_1 = sys.argv[2]
+        cloned_directory_1 = sys.argv[1]
+        git_repo_1 = sys.argv[2]
         print("You have chosen to compare a repo with itself... grabbing the directory...")
         git_importer = GitImporter()
-        git_importer.clone_repo(git_repo_1, cloned_directory_1)
+        git_importer.clone_repo(cloned_directory_1, git_repo_1)
 #       do more stuff here
     elif len(sys.argv) == 5:
-        git_repo_1 = sys.argv[1]
-        cloned_directory_1 = sys.argv[2]
-        git_repo_2 = sys.argv[3]
-        cloned_directory_2 = sys.argv[4]
+        cloned_directory_1 = sys.argv[1]
+        git_repo_1 = sys.argv[2]
+        cloned_directory_2 = sys.argv[3]
+        git_repo_2 = sys.argv[4]
         print("You have chosen to compare two repos... grabbing the repos...")
         git_importer = GitImporter()
-        git_importer.clone_repo(git_repo_1, cloned_directory_1)
-        git_importer.clone_repo(git_repo_2, cloned_directory_2)
+        print("Grabbing the first repo...")
+        git_importer.clone_repo(cloned_directory_1, git_repo_1)
+        print("Grabbing the second repo...")
+        git_importer.clone_repo(cloned_directory_2, git_repo_2)
 #       do more stuff here
 
 
