3b496bf0857866924b663ff6ff5ff5ef7758f143
Denton Wood <Denton_Wood@baylor.edu>
2019-07-01 14:40:11 +0200

    Added structure to main and ignored IDE files

--- venv/main.py
+++ venv/main.py
@@ -1,5 +1,13 @@
 def main():
-    print("Hello world!")
+    # Pull 2 repos from GitHub
+    print("Pulling repositories")
+    # Tokenize repos
+    #       Filter out variable names - only control structures
+    print("Tokenizing repositories")
+    # Use Levenhstein to compare tokens to tokens
+    print("Comparing repositories")
+
+    print("Results:")
 
 
 if __name__ == "__main__":
