ca7626b321e226abdfd5b519ce57bf986051c35b
calebbaylor <caleb_dehaan@baylor.edu>
2019-07-02 09:38:17 +0200

    abstracted the args check

--- venv/main.py
+++ venv/main.py
@@ -3,10 +3,25 @@ from sources.filter import *
 from sources.Levenshtein import *
 from sources.import_repository import *
 
+
+def check_args(argv):
+    if len(argv) != 5 and len(argv) != 3:
+        print("""\
+    Usage:
+        python3 main.py [directory to be cloned to 1] [git repo 1] [directory to be cloned to 2] [git repo 2]
+    or (for comparing within a directory):
+        python3 main.py [directory to be cloned to 1] [git repo 1]
+            """)
+        return False
+    return True
+
+
 def main():
-    # Pull 2 repos from GitHub
-    print("Pulling repositories")
+    flag = check_args(sys.argv)
+    if not flag:
+        return
     import_repository(sys.argv)
+
     # Tokenize repos
     print("Tokenizing repositories")
     list1 = {}
--- venv/sources/import_repository.py
+++ venv/sources/import_repository.py
@@ -2,19 +2,7 @@ from .git_importer import GitImporter
 
 
 def import_repository(argv):
-    for i in argv:
-        print(i)
-    print()
-    print()
-    if len(argv) != 5 and len(argv) != 3:
-        print("""\
-Usage:
-    python3 main.py [directory to be cloned to 1] [git repo 1] [directory to be cloned to 2] [git repo 2]
-or (for comparing within a directory):
-    python3 main.py [directory to be cloned to 1] [git repo 1]
-        """)
-        return
-    elif len(argv) == 3:
+    if len(argv) == 3:
         cloned_directory_1 = argv[1]
         git_repo_1 = argv[2]
         print("You have chosen to compare a repo with itself... grabbing the directory...")
