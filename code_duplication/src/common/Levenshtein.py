85bf95698a1f56539206eebc0aecc930f51c1104
ST3PHANI3 <stephieda98@yahoo.com>
2019-07-02 06:53:35 -0500

    formatting

--- code_duplication/src/common/Levenshtein.py
+++ code_duplication/src/common/Levenshtein.py
@@ -14,7 +14,8 @@ def lev_distance(line1, line2):
     ss1 = line1[15:17]
     ss2 = line2[15:17]
 
-    if ss1 == ss2:
+    # checks if they're equal and not new lines
+    if ss1 == ss2 and ss1 != '4' and ss2 != '4':
         # grab the strings
         i = line1.find("string='")
         j = line1.find("',")
@@ -26,43 +27,31 @@ def lev_distance(line1, line2):
         # THE ACTUAL LEVENSHTEIN ALGORITHM
         length1 = len(w1)+1     # length of word 1 - col
         length2 = len(w2)+1     # length of word 2 - row
-
-        # STEP 1
-        # check if words are empty
+        # STEP 1 - check if words are empty
         if length1-1 == 0 or length2-1 == 0:
             print("cannot compare things - empty")
             exit(1)
-
         # matrix thing
         m = [[0 for x in range(length1)] for y in range(length2)]
-
-        # STEP 2
-        # fill first row & col of matrix
+        # STEP 2 - fill first row & col of matrix
         for x in range(length2):
             m[x][0] = x
-
         for y in range(length1):
             m[0][y] = y
-
         # STEP 3
         for x in range(1, length2):
-
             # STEP 4
             for y in range(1, length1):
-
                 # STEP 5
                 if w1[y-1] == w2[x-1]:
                     cost = 0
                 else:
                     cost = 1
-
                 # STEP 6
                 m[x][y] = min(m[x-1][y]+1, m[x][y-1]+1, m[x-1][y-1]+cost)
-
         # STEP 7
         return m[length2-1][length1-1]
-    else:
-        # returns 3 bc they're definitely not the same
+    else:   # returns 3 bc they're definitely not the same
         return 3
 
 
