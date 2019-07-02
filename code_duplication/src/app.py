2dbde41124a605880b856805bdd15aa27b678ab1
Denton Wood <Denton_Wood@baylor.edu>
2019-07-02 10:45:33 +0200

    Standardized project structure

--- code_duplication/src/app.py
+++ code_duplication/src/app.py
@@ -1,7 +1,7 @@
 import sys
-from sources.filter import *
-from sources.Levenshtein import *
-from sources.import_repository import *
+from code_duplication.src.common.filter import filter_tokens
+from code_duplication.src.common.Levenshtein import lev_distance
+from code_duplication.src.common.import_repository import import_repository
 
 
 # The below function will check to verify that there are the correct number of args
@@ -11,9 +11,9 @@ def check_args(argv):
     if len(argv) != 5 and len(argv) != 3:
         print("""\
     Usage:
-        python3 main.py [directory to be cloned to 1] [git repo 1] [directory to be cloned to 2] [git repo 2]
+        python3 __main__.py [directory to be cloned to 1] [git repo 1] [directory to be cloned to 2] [git repo 2]
     or (for comparing within a directory):
-        python3 main.py [directory to be cloned to 1] [git repo 1]
+        python3 __main__.py [directory to be cloned to 1] [git repo 1]
             """)
         return False
     return True
@@ -34,8 +34,3 @@ def main():
     # Use Levenhstein to compare tokens to tokens
     print("Comparing repositories")
     lev_distance(list1, list2)
-
-
-if __name__ == "__main__":
-    main()
-
