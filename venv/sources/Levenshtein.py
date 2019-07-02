a8b49815927e499829f17bc1777687a6fb8c8b6b
ST3PHANI3 <stephieda98@yahoo.com>
2019-07-02 03:01:54 -0500

    add compare function

--- venv/sources/Levenshtein.py
+++ venv/sources/Levenshtein.py
@@ -11,7 +11,6 @@ def minimum(val1, val2, val3):
 def lev_distance(w1, w2):
     length1 = len(w1)+1 # length of word 1 - col
     length2 = len(w2)+1 # length of word 2 - row
-    cost = 0            # cost
 
     # STEP 1
     # check if words are empty
@@ -32,14 +31,12 @@ def lev_distance(w1, w2):
 
     # STEP 3
     for x in range(1, length2):
-        letter2 = w2[x-1]
 
         # STEP 4
         for y in range(1, length1):
-            letter1 = w1[y-1]
 
             # STEP 5
-            if letter1 == letter2:
+            if w1[y-1] == w2[x-1]:
                 cost = 0
             else:
                 cost = 1
@@ -49,3 +46,17 @@ def lev_distance(w1, w2):
 
     # STEP 7
     return m[length2-1][length1-1]
+
+# takes two lists and compares everything in them to everything in the other one
+def list_compare(list1, list2):
+    un = 0
+    for x in list1:
+        for y in list2:
+            # calculate levenshtein
+            l = lev_distance(x, y)
+            # determine if distance within acceptable threshold
+            if l >= 3:
+                un = un+1
+
+    # calculate percentage of similarity
+    print(un/len(list2)*100)
