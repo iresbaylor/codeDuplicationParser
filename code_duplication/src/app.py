db7b6181af4a1497b93226b952f5e9817aef9e52
natiiix <natiwastaken@gmail.com>
2019-07-10 14:45:09 +0200

    Remove old tokenizer code and replace it with new

--- code_duplication/src/app.py
+++ code_duplication/src/app.py
@@ -1,8 +1,9 @@
+import os
 import sys
 from code_duplication.src.common.filter import filter_tokens
 from code_duplication.src.common.Levenshtein import lev_distance
 from code_duplication.src.common.import_repository import import_repository
-from code_duplication.src.common.time_to_tokenize import *
+from code_duplication.src.common.method_parser import get_methods_from_dir
 from urllib.parse import urlparse
 from code_duplication.src.common.directory_parser import list_files
 import string
@@ -25,7 +26,8 @@ def check_args(argv):
         pieces = urlparse(argv[2])
         url_flag = all([pieces.scheme, pieces.netloc])
         if url_flag:
-            url_flag = set(pieces.netloc) <= set(string.ascii_letters + string.digits + '-.')
+            url_flag = set(pieces.netloc) <= set(
+                string.ascii_letters + string.digits + '-.')
             if url_flag:
                 url_flag = pieces.scheme in ['http', 'https']
         dir_flag = os.path.isdir(argv[1])
@@ -34,13 +36,15 @@ def check_args(argv):
         pieces1 = urlparse(argv[2])
         url_flag1 = all([pieces1.scheme, pieces1.netloc])
         if url_flag1:
-            url_flag1 = set(pieces1.netloc) <= set(string.ascii_letters + string.digits + '-.')
+            url_flag1 = set(pieces1.netloc) <= set(
+                string.ascii_letters + string.digits + '-.')
             if url_flag1:
                 url_flag1 = pieces1.scheme in ['http', 'https']
         pieces2 = urlparse(argv[4])
         url_flag2 = all([pieces2.scheme, pieces2.netloc])
         if url_flag2:
-            url_flag2 = set(pieces2.netloc) <= set(string.ascii_letters + string.digits + '-.')
+            url_flag2 = set(pieces2.netloc) <= set(
+                string.ascii_letters + string.digits + '-.')
             if url_flag2:
                 url_flag2 = pieces2.scheme in ['http', 'https']
         dir_flag1 = os.path.isdir(argv[1])
@@ -64,7 +68,7 @@ def main():
     # ------- FOR TESTING PURPOSES ------------
     if len(sys.argv) >= 2:
         # Find all functions and parse their syntax tree using the TreeNode wrapper
-        for method in get_methods_from_directory(sys.argv[1]):
+        for method in get_methods_from_dir(sys.argv[1]):
             print(method)
 
         return
@@ -75,11 +79,6 @@ def main():
     # if we can set the variables below to the directories we want to compare, it should work
     directory1 = sys.argv[1]
     directory2 = sys.argv[3]
-    # time_to_tokenize_the_directory(directory1, list1)
-    # time_to_tokenize_the_directory(directory2, list2)
-    i_am_groot(directory1)
-    # "./code_duplication/src/common/Levenshtein.py"
-    i_am_groot(directory2)
     # Filter out variable names - only control structures
     print("Filtering repositories")
     filter_tokens(list1, list2)
