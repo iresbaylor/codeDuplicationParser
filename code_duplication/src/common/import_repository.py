974467bc3590d5c4c0b46bc34edd83ee642debf9
natiiix <natiwastaken@gmail.com>
2019-07-10 15:24:06 +0200

    Move git_importer code into repo importing module

--- code_duplication/src/common/import_repository.py
+++ code_duplication/src/common/import_repository.py
@@ -1,16 +1,24 @@
-from .git_importer import GitImporter
+from git import Git
 
+
+def clone_repo(clone_dir, repo_url):
+    """
+    Clones the specfied repository into the specified directory and
+    returns the Repo class instance of the cloned repository.
+    """
+    return Git(clone_dir).clone(repo_url)
 
 # This code will import the repos from git into the specified locations
 # based on the args passed to the program
 # it does not return anything
+
+
 def import_repository(argv):
     if len(argv) == 3:
         cloned_directory_1 = argv[1]
         git_repo_1 = argv[2]
         print("You have chosen to compare a repo with itself... grabbing the directory...")
-        git_importer = GitImporter()
-        git_importer.clone_repo(cloned_directory_1, git_repo_1)
+        clone_repo(cloned_directory_1, git_repo_1)
 #       do more stuff here
     elif len(argv) == 5:
         cloned_directory_1 = argv[1]
@@ -18,9 +26,8 @@ def import_repository(argv):
         cloned_directory_2 = argv[3]
         git_repo_2 = argv[4]
         print("You have chosen to compare two repos... grabbing the repos...")
-        git_importer = GitImporter()
         print("Grabbing the first repo...")
-        git_importer.clone_repo(cloned_directory_1, git_repo_1)
+        clone_repo(cloned_directory_1, git_repo_1)
         print("Grabbing the second repo...")
-        git_importer.clone_repo(cloned_directory_2, git_repo_2)
+        clone_repo(cloned_directory_2, git_repo_2)
 #       do more stuff here
