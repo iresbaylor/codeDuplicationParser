0aac12f52d449cf3be59de2559e3f8f62304e11f
natiiix <natiwastaken@gmail.com>
2019-07-11 15:45:37 +0200

    Implement code for TreeNode list flattening

--- code_duplication/src/common/method_parser.py
+++ code_duplication/src/common/method_parser.py
@@ -2,6 +2,7 @@ import ast
 from os import listdir, path
 from os.path import isdir, isfile
 from .TreeNode import TreeNode
+from collections import deque
 
 
 def _read_whole_file(file_path):
@@ -44,6 +45,34 @@ def _recursive_listdir_py(directory):
     return files
 
 
+def _flatten_node_tree(nodes):
+    node_queue = deque(nodes)
+    flat_nodes = []
+
+    while node_queue:
+        n = node_queue.popleft()
+
+        # Set this node's self-index.
+        n.index = len(flat_nodes)
+
+        # Add this node's index to the list of
+        # children of its parent if it has any.
+        if n.parent_index is not None:
+            flat_nodes[n.parent_index].child_indices.append(n.index)
+
+        # Set this node's children's parent index to this node's index.
+        for c in n.direct_children:
+            c.parent_index = n.index
+
+        # Add this node's children to the queue.
+        node_queue.extend(n.direct_children)
+
+        # Add this node to the list of alraedy visited nodes.
+        flat_nodes.append(n)
+
+    return flat_nodes
+
+
 def get_methods_from_dir(directory):
     """
     Finds all *.py files in the directory recursively.
@@ -56,4 +85,4 @@ def get_methods_from_dir(directory):
     for file_path in _recursive_listdir_py(directory):
         methods.extend(_get_methods_from_file(file_path))
 
-    return methods
+    return methods, _flatten_node_tree(methods)
