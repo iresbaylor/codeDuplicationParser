0d9a41cc0c43d83114455ffd81a18e641b1a8c5c
natiiix <natiwastaken@gmail.com>
2019-07-12 09:00:21 +0200

    Modify TreeNode for easier comparison

--- code_duplication/src/common/TreeNode.py
+++ code_duplication/src/common/TreeNode.py
@@ -4,8 +4,6 @@ import ast
 class TreeNode:
     def __init__(self, node):
         self.node = node
-        # TODO: This should be the actual value in case of childless nodes
-        self.value = node.__class__.__name__
 
         self.all_nodes = list(ast.walk(self.node))
         self.child_nodes = [x for x in self.all_nodes if x is not self.node]
@@ -18,16 +16,46 @@ class TreeNode:
         self.direct_children = [
             TreeNode(x) for x in self.child_nodes if x not in grandchild_nodes]
 
-        self.labels = set([
-            x.id for x in self.all_nodes if isinstance(x, ast.Name)])
+        self.labels = [x.id for x in self.all_nodes if isinstance(x, ast.Name)]
 
-        # These values is set externally after all nodes are parsed
+        # Class name if the node has children, AST dump if it does not.
+        self.value = node.__class__.__name__ if self.direct_children else self.dump()
+
+        if isinstance(node, ast.FunctionDef) and node.name == "pattern_collection":
+            print("asd")
+
+        # These values are set externally after all nodes are parsed
+        # during the node tree flattening process.
         self.index = None
         self.parent_index = None
         self.child_indices = []
 
     def dump(self):
         return ast.dump(self.node)
+
+    def __eq__(self, other):
+        if not isinstance(other, TreeNode):
+            return False
+
+        elif other.dump() == self.dump():
+            return True
+
+        elif other.value != self.value:
+            return False
+
+        elif len(other.direct_children) != len(self.direct_children):
+            return False
+
+        else:
+            for i, v in enumerate(other.direct_children):
+                if not v.__eq__(self.direct_children[i]):
+                    return False
+
+        return True
+
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
     def __str__(self):
         return f"{self.value}[{len(self.direct_children)} children; {len(self.labels)} labels; index={self.index}; parent={self.parent_index}; children={self.child_indices}]"
