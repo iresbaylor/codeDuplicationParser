7ec875d32f40e1205f2cd908992cda7f8753d2cd
Denton Wood <Denton_Wood@baylor.edu>
2019-07-15 11:57:28 +0200

    Added bitarray functionality

--- code_duplication/src/common/pattern_collection.py
+++ code_duplication/src/common/pattern_collection.py
@@ -1,32 +1,34 @@
-"""
-Compares two Abstract Syntax Trees representing two methods
-"""
+from bitarray import bitarray
 
 
-def pattern_collection(tree1, tree2, labels1, labels2):
+def pattern_collection(tree_list_1, tree_list_2):
+    """
+    Compares two Abstract Syntax Trees representing two methods
+    """
+    # Get lengths
+    len_tree_1 = len(tree_list_1)
+    len_tree_2 = len(tree_list_2)
+
     # Checks whether a pairing has been compared
-    worklist = [[True] * len(labels2) for i in range(len(labels1))]
-    # Get subtrees from function 1
-    node_list_1 = tree1.funcdef.walk()
-    # Get subtrees from function 2
-    node_list_2 = tree2.funcdef.walk()
+    bit_string = '1' * len(tree_list_2)
+    work_list = [bitarray(bit_string, endian='little') for i in range(len(tree_list_1))]
 
-    # map of s to 2^p
-    pats = [[] * (len(node_list_1) + len(node_list_2))]
-    # map of s to 2^c
-    cpats = [[] * (len(node_list_1) + len(node_list_2))]
+    # minimum common subtrees (patterns) of trees
+    pats = []
+    # clustered patterns
+    cpats = []
 
     # for all subtrees that aren't leaf nodes
-    for i in range(0, len(node_list_1)):
-        for j in range(0, len(node_list_2)):
-            # if worklist is true
-            if worklist[i][j]:
+    for i in range(len_tree_1):
+        for j in range(len_tree_2):
+            # if work_list is true
+            if work_list[i][j]:
                 # set it to false
-                worklist[i][j] = False
+                work_list[i][j] = False
                 # if the root nodes of the subtrees are equal
-                if node_list_1[i].equals(node_list_2[j]):
+                if tree_list_1[i] == tree_list_2[j]:
                     # Add the results of anti-unify to the list of subtrees
-                    pats[i].append(anti_unify(node_list_1[i], node_list_2[i], worklist))
+                    pats[i].append(anti_unify(tree_list_1, tree_list_2, i, j, work_list))
     # for every node in the tree
     for i in range(0, len(pats)):
         # Run the clustering function on the pats of each element
@@ -35,9 +37,9 @@ def pattern_collection(tree1, tree2, labels1, labels2):
     return cpats
 
 
-def anti_unify(subtree1, subtree2, worklist):
-    return 0
+def anti_unify(tree_list_1, tree_list_2, index_1, index_2, worklist):
+    return []
 
 
 def clustering(pattern):
-    return 0
+    return []
