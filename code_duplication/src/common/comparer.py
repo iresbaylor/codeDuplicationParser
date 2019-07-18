abdc7b20a92eeaacdb33430ff4f4ab8cbda10eea
natiiix <natiwastaken@gmail.com>
2019-07-18 10:09:01 +0200

    Implement two-repo comparer using my alt algorithm

--- code_duplication/src/common/comparer.py
+++ code_duplication/src/common/comparer.py
@@ -1,8 +1,27 @@
 from .repo_cloner import _clone_repo
 from .module_parser import get_modules_from_dir
+from .benchmark import time_snap
 
 _MIN_WEIGHT = 80
 _MIN_MATCH_PERCENTAGE = 80
+
+
+def _flatten(list_of_lists):
+    flat = []
+
+    for l in list_of_lists:
+        flat.extend(l)
+
+    return flat
+
+
+def _get_all_children(node):
+    children = node.children
+
+    for c in children:
+        children.extend(_get_all_children(c))
+
+    return children
 
 
 def _get_skeleton(node_value, child_skeletons):
@@ -41,10 +60,13 @@ def _type1_compare(node1, node2):
 
 
 def find_clones_in_repo(repo_url):
+    time_snap("Start of function")
     repo_dir = _clone_repo(repo_url)
+    time_snap("Repository cloned")
     repo_modules = get_modules_from_dir(repo_dir)
 
     nodes = [m[0] for m in repo_modules]
+    time_snap("Modules / nodes parsed")
 
     ignore_dict = {}
     start = 0
@@ -70,7 +92,7 @@ def find_clones_in_repo(repo_url):
                     continue
 
                 total_weight = n1.weight + n2.weight
-                if total_weight < 50:
+                if total_weight < _MIN_WEIGHT:
                     continue
 
                 match_weight, match_skeleton = _type1_compare(n1, n2)
@@ -94,3 +116,62 @@ def find_clones_in_repo(repo_url):
                     ignore_dict[index] = ignore_set.copy()
 
         start = end
+
+    time_snap("End of function")
+
+
+def compare_two_repos(repo1_url, repo2_url):
+    time_snap("Function start")
+    repo1_dir = _clone_repo(repo1_url)
+    time_snap("Clone first")
+    repo2_dir = _clone_repo(repo2_url)
+    time_snap("Clone second")
+    repo1_modules = get_modules_from_dir(repo1_dir)
+    time_snap("Get modules from first")
+    repo2_modules = get_modules_from_dir(repo2_dir)
+    time_snap("Get modules from second")
+    repo1_nodes = [m[0] for m in repo1_modules]
+    repo2_nodes = _flatten(repo2_modules)
+    time_snap("Convert modules into nodes")
+
+    ignore_dict = {}
+    start = 0
+
+    while start < len(repo1_nodes):
+        end = len(repo1_nodes)
+
+        for i1 in range(start, end):
+            n1 = repo1_nodes[i1]
+            ignore_set = ignore_dict.pop(i1, set())
+
+            for n2 in repo2_nodes:
+                if not _can_be_compared(n1, n2):
+                    continue
+
+                total_weight = n1.weight + n2.weight
+                if total_weight < _MIN_WEIGHT:
+                    continue
+
+                match_weight, match_skeleton = _type1_compare(n1, n2)
+                if not match_weight:
+                    continue
+
+                match_percentage = round(match_weight / total_weight * 100, 2)
+
+                if match_percentage >= _MIN_MATCH_PERCENTAGE:
+                    print(f"\n{match_skeleton}\n\n{n1}\n{n2}\n" +
+                          f"Similarity: {match_percentage:g} % ({match_weight} out of {total_weight} nodes)")
+
+                if match_weight == total_weight:
+                    ignore_set.update(n2.children)
+
+            first_index = len(repo1_nodes)
+            repo1_nodes.extend(n1.children)
+
+            if ignore_set:
+                for i in range(first_index, len(repo1_nodes)):
+                    ignore_dict[i] = ignore_set.copy()
+
+        start = end
+
+    time_snap("End of function")
