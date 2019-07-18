0b07db537fac7fc8909e6361b50e9eda98dd4d50
Denton Wood <Denton_Wood@baylor.edu>
2019-07-18 14:29:49 +0200

    if statement was always failing

--- code_duplication/src/common/primary_algorithm/pattern_collection.py
+++ code_duplication/src/common/primary_algorithm/pattern_collection.py
@@ -31,7 +31,7 @@ def pattern_collection(tree_list_1, tree_list_2):
     for i in range(size_tree_1):
         for j in range(size_tree_2):
             # if neither tree is a leaf node and the pair hasn't been checked
-            if tree_list_1[i].children and tree_list_2[j].children and work_list[i][j]:
+            if tree_list_1[i].children and tree_list_2[j].children and not work_list[i][j]:
                 # we have now checked this pairing
                 work_list[i][j] = True
                 # if the root nodes of the subtrees are equal
