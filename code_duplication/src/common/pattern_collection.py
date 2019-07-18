670a08c60035e21965422be3325ebd6814c1b52e
Denton Wood <Denton_Wood@baylor.edu>
2019-07-18 10:11:08 +0200

    Added check for leaf nodes and documentation

--- code_duplication/src/common/pattern_collection.py
+++ code_duplication/src/common/pattern_collection.py
@@ -4,37 +4,38 @@ from .pattern_clustering import clustering
 
 def pattern_collection(tree_list_1, tree_list_2):
     """
-    Compares two Abstract Syntax Trees representing two methods
+    Compares two Abstract Syntax Trees representing two methods. The trees are
+    provided as lists to provides indexes for the nodes within the tree
     """
-    # Get lengths
-    len_tree_1 = len(tree_list_1)
-    len_tree_2 = len(tree_list_2)
+    # Get the sizes of the trees
+    size_tree_1 = len(tree_list_1)
+    size_tree_2 = len(tree_list_2)
 
     # Checks whether a pairing has been compared
     bit_string = '1' * len(tree_list_2)
     work_list = [bitarray(bit_string, endian='little') for _ in tree_list_1]
 
-    # minimum common subtrees (patterns) of trees
-    pats = [[] for i in range(len_tree_1)]
-    # clustered patterns
+    # sets of minimum common subtrees (patterns) of trees
+    pats = [[] for i in range(size_tree_1)]
+    # sets of patterns which have been clustered together
     cpats = []
 
-    # for all subtrees that aren't leaf nodes
-    for i in range(len_tree_1):
-        for j in range(len_tree_2):
-            # if work_list is true
-            if work_list[i][j]:
-                # set it to false
+    # for all subtrees of both trees
+    for i in range(size_tree_1):
+        for j in range(size_tree_2):
+            # if neither tree is a leaf node and the pair hasn't been checked
+            if tree_list_1[i].children and tree_list_2[j].children and work_list[i][j]:
+                # we have now checked this pairing
                 work_list[i][j] = False
                 # if the root nodes of the subtrees are equal
                 if tree_list_1[i] == tree_list_2[j]:
                     # Add the results of anti-unify to the list of subtrees
                     pats[i].append(anti_unify(tree_list_1, tree_list_2, i, j, work_list))
-    # for every node in the tree
-    for pattern in pats:
-        # Run the clustering function on the pats of each element
-        cpats.append(clustering(pattern))
-    # Return the values of the clustering function
+    # for every set of patterns (one per node in the first tree)
+    for pattern_set in pats:
+        # run the clustering function on the pattern set
+        cpats.append(clustering(pattern_set))
+    # Return the sets of clustered patterns
     return cpats
 
 
