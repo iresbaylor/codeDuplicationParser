624717b6279cc1e6893c0d38ee0655c0071088b3
ST3PHANI3 <stephieda98@yahoo.com>
2019-07-18 03:10:19 -0500

    anti_unify function

---
+++ code_duplication/src/common/anti_unification.py
@@ -0,0 +1,28 @@
+from .PatternNode import PatternNode
+
+
+def anti_unify(list1, list2, index1, index2, worktable):
+    # mark the pair as checked/True <-- really necessary??
+    worktable[index1][index2] = True
+    # determine if subtrees are the same (and lengths same)
+    if list1 == list2:  # if true:
+        # check if leaves
+        if not list1[index1].child_indices and not list2[index2].child_indices:
+            t = PatternNode(list1[index1], list2[index2], list1[index1].weight, list1[index1].value)
+            return t
+        else:  # if not leaves:
+            # iterate thru node's children, adding them as new children to the new fake node
+            p = PatternNode(list1[index1], list2[index2], list1[index1].weight)
+            for c in list1[index1].child_indices:
+                # call function on children to get their subtrees - DFS???
+                # RECURSION
+                subtree = anti_unify(list1, list2, c, c, worktable)
+                # smoosh the subtree into a list??? might already be one
+                # associate with p
+                p.add_children(subtree)
+            return p
+    else:  # if false:
+        # return hole with combined label
+        weights = [list1[index1].weight, list2[index2].weight]
+        temp = PatternNode(list1[index1], list2[index2], weights)
+        return temp
