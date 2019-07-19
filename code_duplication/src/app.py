cd244f15348220299a47e1c1a07895fde6ea3a7e
Denton Wood <Denton_Wood@baylor.edu>
2019-07-19 10:22:23 +0200

    Moved debug printing functions to their own file

--- code_duplication/src/app.py
+++ code_duplication/src/app.py
@@ -96,17 +96,3 @@ def type1_check(modules):
     for v in node_dict.values():
         if len(v) > 1:
             print(v)
-
-
-def print_node_list(node_list):
-    for node in node_list:
-        if node.parent_index is None:
-            print_node(node, "", 0, node_list)
-
-
-def print_node(node, indent, level, node_list):
-    print(indent, "(", level, ")", node)
-    for index in node.child_indices:
-        for node in node_list:
-            if node.index == index:
-                print_node(node, indent + "    ", level + 1, node_list)
