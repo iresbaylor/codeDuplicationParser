0a2b51c9329cd62e6f140237cbf1c7f1af709234
natiiix <natiwastaken@gmail.com>
2019-07-23 17:17:00 +0200

    Add docstring to DetectionResult

--- code_duplication/src/results/DetectionResult.py
+++ code_duplication/src/results/DetectionResult.py
@@ -2,6 +2,16 @@ from json import dumps as json_dumps
 
 
 class DetectionResult:
+    """
+    Represents the final result of a detection query.
+
+    For now it only contains a list of detected clones,
+    but more information may be added in the future.
+
+    Attributes:
+        clones {list[DetectedClone]} -- List of detected code clones.
+    """
+
     def __init__(self, clones):
         """
         Initializes a new detection result given the list of detected clones.
