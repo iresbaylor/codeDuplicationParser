e5a3495bb819aa6641bbd8d16e3041f000efd56f
natiiix <natiwastaken@gmail.com>
2019-07-23 16:59:55 +0200

    Add docstring to DetectedClone

--- code_duplication/src/output/DetectedClone.py
+++ code_duplication/src/output/DetectedClone.py
@@ -1,4 +1,16 @@
 class DetectedClone:
+    """
+    Represents a single detected code clone.
+
+    Attributes:
+        value {string} -- String representation common to all the nodes.
+        similarity {float} -- Similarity coefficient in range from 0 to 1,
+                              where 0 means completely different and
+                              1 means the nodes are all exactly equal.
+        weight {int} -- Total weight of all the origin nodes.
+        origins {list[string]} -- Origins of all nodes involved in this clone.
+    """
+
     def __init__(self, value, similarity, nodes):
         """
         Initializes a new detected clone
