458577fb6ebd6ac5272edfa20bc0fbf38773137c
natiiix <natiwastaken@gmail.com>
2019-07-23 10:01:10 +0200

    Slightly improve web UI and its server

--- web/app.py
+++ web/app.py
@@ -1,29 +1,39 @@
-from flask import Flask
-from psycopg2 import connect, Error as PG_Error
+from flask import Flask, request
+# from psycopg2 import connect, Error as PG_Error
 from sys import stderr
-from .credentials import conn_str
-# from code_duplication import ???
+# from .credentials import conn_str
+from code_duplication.src.secondary_algorithm.fast_check import type1_check_repo
+import os.path
+
+_INDEX_HTML = os.path.join(os.path.dirname(__file__), "index.html")
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    try:
-        conn = connect(conn_str)
+    with open(_INDEX_HTML, "r", encoding="utf-8") as f:
+        webpage = f.read()
 
-        cur = conn.cursor()
+    first_repo = request.args.get("first")
 
-        cur.execute("SELECT col FROM test;")
-        return str(cur.fetchall())
+    return webpage.replace("#LOG#", "\n\n".join([f"{k[:20]} -- {v}" for k, v in type1_check_repo(first_repo, 30).items()]) if first_repo else "")
 
-    except PG_Error as ex:
-        print("PostgreSQL Error:", ex, file=stderr)
+    # try:
+    #     conn = connect(conn_str)
 
-    finally:
-        if conn:
-            cur.close()
-            conn.close()
+    #     cur = conn.cursor()
+
+    #     cur.execute("SELECT col FROM test;")
+    #     return str(cur.fetchall())
+
+    # except PG_Error as ex:
+    #     print("PostgreSQL Error:", ex, file=stderr)
+
+    # finally:
+    #     if conn:
+    #         cur.close()
+    #         conn.close()
 
 
 if __name__ == "__main__":
