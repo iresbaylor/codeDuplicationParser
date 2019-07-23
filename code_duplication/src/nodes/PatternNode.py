1f54610295003b8915676b3b243217703fa510b9
natiiix <natiwastaken@gmail.com>
2019-07-23 17:39:32 +0200

    Remove to_json() from PatternNode
    This is no longer needed thanks to the results classes.

--- code_duplication/src/nodes/PatternNode.py
+++ code_duplication/src/nodes/PatternNode.py
@@ -64,18 +64,6 @@ class PatternNode:
 
         return True
 
-    def to_json(self):
-        """
-        Converts the node into its JSON representation
-        for easier use by other tools.
-
-        Returns:
-            string -- JSON representation of the node.
-        """
-
-        return json_dumps({"value": self.value,
-                           "origins": [n.origin for n in self.nodes]})
-
     def __str__(self):
         return f"{self.value}(', '.join{[n.origin for n in self.nodes]})"
 
