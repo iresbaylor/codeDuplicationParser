f2faa27ba1bc3e6927c66392998c4180a46e6a4a
natiiix <natiwastaken@gmail.com>
2019-07-24 18:58:42 +0200

    Add func for easily getting modules from repo path

--- code_duplication/src/preprocessing/module_parser.py
+++ code_duplication/src/preprocessing/module_parser.py
@@ -3,7 +3,7 @@ from os import listdir, path
 from os.path import isdir, isfile
 from ..nodes.TreeNode import TreeNode
 from collections import deque
-from .repo_cloner import clone_root_dir
+from .repo_cloner import clone_root_dir, get_repo_dir
 
 
 def _read_whole_file(file_path):
@@ -117,3 +117,19 @@ def get_modules_from_dir(directory):
 
     return [_flatten_module_nodes(_get_tree_node_from_file(f))
             for f in _recursive_listdir_py(directory)]
+
+
+def get_modules_from_repo(repo):
+    """
+    Just a shorthand for `get_modules_from_dir(get_repo_dir(repo))`.
+    Clones the repository or finds its directory and then finds
+    all modules inside of that directory and returns them.
+
+    Arguments:
+        repo {string} -- Repository path.
+
+    Returns:
+        list[list[TreeNode]] -- List of lists of nodes from parsed modules.
+    """
+
+    return get_modules_from_dir(get_repo_dir(repo))
