7d0b04599d95a061fd6f6c96507df931446eccf4
natiiix <natiwastaken@gmail.com>
2019-07-24 18:16:54 +0200

    Add checks to repo_cloner to avoid weird behavior

--- code_duplication/src/preprocessing/repo_cloner.py
+++ code_duplication/src/preprocessing/repo_cloner.py
@@ -1,4 +1,4 @@
-from git import Git, Repo, InvalidGitRepositoryError
+from git import Git, Repo, InvalidGitRepositoryError, GitCommandError
 from os import path, makedirs
 from os.path import isdir, dirname
 from urllib.parse import urlparse
@@ -15,28 +15,41 @@ def _clone_repo(repo_url):
     """
     Clones the specified repository into a special internal directory and
     returns the directory path of the cloned repository.
+
+    Returns:
+        string -- Cloned repository directory on success; None on failure.
     """
 
     # Make sure the base clone dir exists.
     makedirs(clone_root_dir, exist_ok=True)
 
-    repo_name = re.sub(r"^.*?/([^/]+?)(?:\.git)?/?$",
-                       r"\1", urlparse(repo_url).path)
-    repo_dir = path.join(clone_root_dir, repo_name)
+    # NOTE: Only standard GitHub and GitLab are currently properly supported.
+    match = re.fullmatch(
+        r"^(?:https?://)?(?:[\w\-\.]*\.)?([\w\-]+)\.\w{1,10}/([\w\-]+)/([\w\-]+)(?:/?\.git)?/?$", repo_url)
 
-    try:
-        # If repo dir already exists, pull
-        if isdir(repo_dir):
-            Repo(repo_dir).remotes.origin.pull()
-        # Otherwise, clone the repo
-        else:
-            Git(clone_root_dir).clone(repo_url)
-
-        return repo_dir
-
-    # If anything goes wrong with the repo
-    except InvalidGitRepositoryError:
+    if not match:
         return None
+
+    repo_dir = path.join(clone_root_dir, match[1], match[2], match[3])
+
+    # If repo dir already exists, pull it.
+    if isdir(repo_dir):
+        try:
+            Repo(repo_dir).remotes.origin.pull()
+        except InvalidGitRepositoryError:
+            return None
+
+    # If the repo hasn't been cloned yet, clone it.
+    else:
+        # NOTE: Repo.clone_from() seems to return a Repo instance.
+        # That might be useful for something later on.
+
+        try:
+            Repo.clone_from(repo_url, repo_dir)
+        except GitCommandError:
+            return None
+
+    return repo_dir
 
 
 def get_repo_dir(repo):
@@ -53,24 +66,30 @@ def get_repo_dir(repo):
         string -- Local path to the repository's directory.
     """
 
-    # Local directory path
-    if config.allow_local_access and isdir(repo):
-        return repo
+    # TODO: This option should probably be removed in the future.
+    # It is more confusing than it is practical now.
 
-    # Name of a previously cloned repository
+    # Path of a previously cloned repository: "[server]/[user]/[repo name]"
     repo_dir_by_name = path.join(clone_root_dir, repo)
-    if re.fullmatch(r"^[\w\-]+$", repo) and isdir(repo_dir_by_name):
+    if re.fullmatch(r"^[\w\-]+/[\w\-]+/[\w\-]+$", repo) and isdir(repo_dir_by_name):
         return repo_dir_by_name
 
     # Shorthand for GitHub URLs: "[repository owner]/[repository name]"
     if re.fullmatch(r"^[\w\-]+/[\w\-]+(?:\.git)$", repo):
         repo_dir = _clone_repo("https://github.com/" + repo)
+
         if repo_dir:
             return repo_dir
 
-    # NOTE: GitPython will pretend it has cloned a repository
-    # even if no such repository existed. In reality,
-    # it will just initialize a new local Git repository.
+    # Local directory path
+    if isdir(repo):
+        if config.allow_local_access:
+            return repo
+        else:
+            raise UserInputError(
+                f"Access to local directory denied: \"{repo}\"")
+
+    # Full remote repository URL
     repo_dir = _clone_repo(repo)
     if repo_dir:
         return repo_dir
