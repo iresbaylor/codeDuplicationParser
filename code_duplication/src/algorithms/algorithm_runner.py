780fbcddab78c290b7804836e12cc6c4ef9e8066
natiiix <natiwastaken@gmail.com>
2019-07-24 19:11:10 +0200

    Add single-repo chlorine to algorithm runner

--- code_duplication/src/algorithms/algorithm_runner.py
+++ code_duplication/src/algorithms/algorithm_runner.py
@@ -1,9 +1,12 @@
 from .oxygen.oxygen import oxygen
+from .chlorine.chlorine import chlorine_single_repo, chlorine_two_repos
 from ..errors.UserInputError import UserInputError
 
 
 def run_single_repo(modules, algorithm):
     if algorithm == "oxygen":
         return oxygen(modules)
+    elif algorithm == "chlorine":
+        return chlorine_single_repo(modules)
     else:
         raise UserInputError(f"Invalid algorithm name: \"{algorithm}\"")
