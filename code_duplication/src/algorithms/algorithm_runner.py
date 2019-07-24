16bbe4ab55b81057bbc09ddd78ebd5d8f1e5c1be
natiiix <natiwastaken@gmail.com>
2019-07-24 19:16:55 +0200

    Add docstrings to algorithm_runner

--- code_duplication/src/algorithms/algorithm_runner.py
+++ code_duplication/src/algorithms/algorithm_runner.py
@@ -4,6 +4,20 @@ from ..errors.UserInputError import UserInputError
 
 
 def run_single_repo(modules, algorithm):
+    """
+    Runs the specified code clone detection algorithm on a single repository.
+
+    Arguments:
+        modules {list[list[TreeNode]]} -- List of the repo's modules.
+        algorithm {string} -- Code clone detection algorithm to use.
+
+    Raises:
+        UserInputError -- If the algorithm name is invalid.
+
+    Returns:
+        DetectionResult -- Result of the code clone detection.
+    """
+
     if algorithm == "oxygen":
         return oxygen(modules)
     elif algorithm == "chlorine":
@@ -13,6 +27,21 @@ def run_single_repo(modules, algorithm):
 
 
 def run_two_repos(modules1, modules2, algorithm):
+    """
+    Runs the specified code clone detection algorithm on two repositores.
+
+    Arguments:
+        modules1 {list[list[TreeNode]]} -- List of first repo's modules.
+        modules2 {list[list[TreeNode]]} -- List of second repo's modules.
+        algorithm {string} -- Code clone detection algorithm to use.
+
+    Raises:
+        UserInputError -- If the algorithm name is invalid.
+
+    Returns:
+        DetectionResult -- Result of the code clone detection.
+    """
+
     if algorithm == "chlorine":
         return chlorine_two_repos(modules1, modules2)
     else:
