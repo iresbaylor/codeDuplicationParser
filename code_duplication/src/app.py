0e4895530654084955dde070cd2a4c8f18b17756
natiiix <natiwastaken@gmail.com>
2019-07-24 19:01:38 +0200

    Remove fast analysis from main()

--- code_duplication/src/app.py
+++ code_duplication/src/app.py
@@ -3,7 +3,6 @@ from .preprocessing.args_handler import handle_args
 from .preprocessing.module_parser import get_modules_from_dir
 from .algorithms.iodine.iodine import iodine
 from .utils.benchmark import time_snap
-from .secondary_algorithm.fast_check import type1_check
 from fastlog import log
 from .errors.UserInputError import UserInputError
 
@@ -36,13 +35,6 @@ def main():
             raise UserInputError(f"Second repository is empty: \"{repos[1]}\"")
 
         time_snap("Parsed second repository")
-
-        log.info("Beginning fast analysis...")
-        type1_check(module_list_1)
-        time_snap("Type 1 check for first repository")
-
-        type1_check(module_list_2)
-        time_snap("Type 1 check for second repository")
 
         log.info("Beginning full analysis...")
         iodine(module_list_1, module_list_2)
