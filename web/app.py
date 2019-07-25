df1096e3f34ce3be99e5787d32e737f032b55b8c
natiiix <natiwastaken@gmail.com>
2019-07-25 10:20:21 +0200

    Update web app to use new HTML files

--- web/app.py
+++ web/app.py
@@ -11,17 +11,23 @@ from engine.errors.UserInputError import UserInputError
 # Disable access to local file system
 config.allow_local_access = False
 
-_INDEX_HTML = os.path.join(os.path.dirname(__file__), "index.html")
-
 app = Flask(__name__)
+
+
+def _read_html(file_name):
+    file_path = os.path.join(os.path.dirname(__file__), file_name + ".html")
+    with open(file_path, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+_INDEX_HTML = _read_html("index")
+_MESSAGE_HTML = _read_html("message")
+_RESULTS_HTML = _read_html("results")
 
 
 @app.route("/")
 def hello():
-    with open(_INDEX_HTML, "r", encoding="utf-8") as f:
-        webpage = f.read()
-
-    output = ""
+    content = ""
 
     repo = request.args.get("repo")
     if repo:
@@ -29,14 +35,21 @@ def hello():
             modules = get_modules_from_repo(repo)
             result = run_single_repo(modules, "oxygen")
 
-            output = "<ol>" + "".join([("<li>" + c.value + f" - Weight: {c.match_weight}" + "<ul>" +
-                                        "".join(["<li>" + orig + f" - Similarity: {sim * 100:g} %" + "</li>" for orig, sim in c.origins.items()]) +
-                                        "</ul></li>") for c in result.clones]) + "</ol>"
+            if result.clones:
+                clones = "<ol>" + "".join([("<li>" + c.value + f" - Weight: {c.match_weight}" + "<ul>" +
+                                            "".join(["<li>" + orig + f" - Similarity: {sim * 100:g} %" + "</li>" for orig, sim in c.origins.items()]) +
+                                            "</ul></li>") for c in result.clones]) + "</ol>"
+
+                content = _RESULTS_HTML.replace("#CLONES#", clones)
+
+            else:
+                content = _MESSAGE_HTML.replace(
+                    "#MSG#", "<h4>No code clones detected. Congratulations!</h4>")
 
         except UserInputError as ex:
-            output = ex.message
+            content = _MESSAGE_HTML.replace("#MSG#", ex.message)
 
-    return webpage.replace("#LOG#", output)
+    return _INDEX_HTML.replace("#CONTENT#", content)
 
     # try:
     #     conn = connect(conn_str)
