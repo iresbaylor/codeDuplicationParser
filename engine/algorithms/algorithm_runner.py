01f599fb4af4745748b12e0628200a63d4695f21
Ivo Meixner <natiwastaken@gmail.com>
2019-08-05 17:45:41 +0200

    Make UserInputError import absolute

--- engine/algorithms/algorithm_runner.py
+++ engine/algorithms/algorithm_runner.py
@@ -3,7 +3,7 @@
 from .oxygen.oxygen import oxygen
 from .chlorine.chlorine import chlorine_single_repo, chlorine_two_repos
 from .iodine.iodine import iodine
-from ..errors.user_input import UserInputError
+from engine.errors.user_input import UserInputError
 from . import OXYGEN, IODINE, CHLORINE
 
 
