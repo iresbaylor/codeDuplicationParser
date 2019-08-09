6a947a613f2a8cee0448a661e34b806f2dccf629
Ivo Meixner <natiwastaken@gmail.com>
2019-08-09 16:43:42 +0200

    Add not-None test for Chlorine (single-repo)

---
+++ test/test_chlorine_single.py
@@ -0,0 +1,16 @@
+"""Module containing tests for single-repo mode of the Chlorine algorithm."""
+
+from . import test_repo1_dir
+from engine.preprocessing.module_parser import get_modules_from_dir
+from engine.algorithms.chlorine.chlorine import chlorine_single_repo
+
+
+def test_chlorine_single_not_none():
+    modules = get_modules_from_dir(test_repo1_dir)
+
+    assert modules
+
+    result = chlorine_single_repo(modules)
+
+    assert result
+    assert result.clones
