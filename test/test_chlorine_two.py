d5073cde4c58b316250c38c6fdf5ee1125fbd744
Ivo Meixner <natiwastaken@gmail.com>
2019-08-09 16:43:48 +0200

    Add not-None test for Chlorine (two-repo)

---
+++ test/test_chlorine_two.py
@@ -0,0 +1,18 @@
+"""Module containing tests for two-repo mode of the Chlorine algorithm."""
+
+from . import test_repo1_dir, test_repo2_dir
+from engine.preprocessing.module_parser import get_modules_from_dir
+from engine.algorithms.chlorine.chlorine import chlorine_two_repos
+
+
+def test_chlorine_two_not_none():
+    modules1 = get_modules_from_dir(test_repo1_dir)
+    assert modules1
+
+    modules2 = get_modules_from_dir(test_repo2_dir)
+    assert modules2
+
+    result = chlorine_two_repos(modules1, modules2)
+
+    assert result
+    assert result.clones
