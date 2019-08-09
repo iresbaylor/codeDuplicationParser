e686909776d7458821dd8d7f57aeec3ad8e76d47
Ivo Meixner <natiwastaken@gmail.com>
2019-08-09 16:34:02 +0200

    Use test repo path straight from test module

--- test/test_module_parser.py
+++ test/test_module_parser.py
@@ -1,12 +1,12 @@
-from os.path import dirname, join as path_join
-from test import __file__ as tests_dir_path
+"""Module containing unit tests for the module parser."""
+
+from . import test_repo1_dir
 from engine.preprocessing.module_parser import get_modules_from_dir
 from engine.nodes.tree import TreeNode
 
 
 def test_get_modules():
-    modules = get_modules_from_dir(
-        path_join(dirname(tests_dir_path), "repos/CodeDuplicateTest1"))
+    modules = get_modules_from_dir(test_repo1_dir)
 
     assert isinstance(modules, list)
     assert len(modules) > 0
