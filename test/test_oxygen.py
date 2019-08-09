ec720f366087e2cb2f7032cf88a8ae2b867e1a4e
Ivo Meixner <natiwastaken@gmail.com>
2019-08-09 16:37:49 +0200

    Add test to make sure Oxygen returns anything
    Basically this just makes sure None isn't returned as a result
    when given a valid list of modules.

---
+++ test/test_oxygen.py
@@ -0,0 +1,14 @@
+from . import test_repo1_dir
+from engine.preprocessing.module_parser import get_modules_from_dir
+from engine.algorithms.oxygen.oxygen import oxygen
+
+
+def test_oxygen_not_none():
+    modules = get_modules_from_dir(test_repo1_dir)
+
+    assert modules
+
+    result = oxygen(modules)
+
+    assert result
+    assert result.clones
