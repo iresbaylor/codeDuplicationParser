c6893e787fb1b68e500fcac7fd686bffcd5678b3
Ivo Meixner <natiwastaken@gmail.com>
2019-08-09 16:33:38 +0200

    Add test repo paths directly to test module

--- test/__init__.py
+++ test/__init__.py
@@ -1,1 +1,9 @@
 """Package containing tests for all parts of this application (engine, interfaces)."""
+
+from os.path import dirname, join as path_join
+
+_test_dir = dirname(__file__)
+_test_repos_dir = path_join(_test_dir, "repos")
+
+test_repo1_dir = path_join(_test_repos_dir, "CodeDuplicateTest1")
+test_repo2_dir = path_join(_test_repos_dir, "CodeDuplicateTest2")
