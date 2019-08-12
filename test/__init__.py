81965de47af83b98e3dd5a427a664fdd8e3ffe5b
Ivo Meixner <natiwastaken@gmail.com>
2019-08-12 15:01:14 +0200

    Add test repo URLs to test module's namespace

--- test/__init__.py
+++ test/__init__.py
@@ -7,3 +7,6 @@ _test_repos_dir = path_join(_test_dir, "repos")
 
 test_repo1_dir = path_join(_test_repos_dir, "CodeDuplicateTest1")
 test_repo2_dir = path_join(_test_repos_dir, "CodeDuplicateTest2")
+
+test_repo1_url = "https://github.com/SchaefferDuncan/CodeDuplicateTest1"
+test_repo2_url = "https://github.com/SchaefferDuncan/CodeDuplicateTest2"
