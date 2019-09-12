354924f2e7812102d7d2168e40ee069fc0722944
Ivo Meixner <natiwastaken@gmail.com>
2019-09-12 13:50:15 +0200

    Add data for single-func-def test

---
+++ test/data/single_func_def/reference/__main__.py
@@ -0,0 +1,8 @@
+def hello(param1="Wor", arg2="ld!"):
+    """The `hello()` test function."""
+    combined_args = param1 + arg2
+
+    prefix = "Hello"
+    prefix += ","
+
+    print(prefix, combined_args)
