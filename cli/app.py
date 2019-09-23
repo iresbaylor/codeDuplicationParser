294dbf14c825def5da5c2e6dc8956bdba41ff76e
Ivo Meixner <natiwastaken@gmail.com>
2019-09-23 10:18:12 +0200

    Add single-repo mode to CLI; Fix #107

--- cli/app.py
+++ cli/app.py
@@ -1,11 +1,10 @@
 """Module containing the CLI's core logic."""
 
 import sys
-import os
 from datetime import datetime
 from .args_handler import handle_cli_args
 from engine.preprocessing.module_parser import get_modules_from_dir
-from engine.algorithms.algorithm_runner import run_two_repos
+from engine.algorithms.algorithm_runner import run_single_repo, run_two_repos
 from engine.utils.benchmark import time_snap
 from fastlog import log
 from engine.errors.user_input import UserInputError
@@ -17,35 +16,37 @@ def main():
         # Parse command line arguments
         repos, algorithm = handle_cli_args()
 
-        time_snap("Cloned repositories")
+        time_snap("Arguments handled; Repositories parsed")
 
-        # Find all functions and parse their syntax tree using the TreeNode wrapper
-        log.info("Parsing methods in repositories...")
         module_list_1 = get_modules_from_dir(repos[0])
+        time_snap("First repository modules parsed")
 
         if not module_list_1:
             raise UserInputError(f"First repository is empty: \"{repos[0]}\"")
 
-        time_snap("Parsed first repository")
+        if repos[1]:
+            module_list_2 = get_modules_from_dir(repos[1])
+            time_snap("Second repository modules parsed")
 
-        module_list_2 = get_modules_from_dir(repos[1])
+            if not module_list_2:
+                raise UserInputError(
+                    f"Second repository is empty: \"{repos[1]}\"")
 
-        if not module_list_2:
-            raise UserInputError(f"Second repository is empty: \"{repos[1]}\"")
+            log.info("Running a two-repository analysis...")
+            result = run_two_repos(module_list_1, module_list_2, algorithm)
 
-        time_snap("Parsed second repository")
+        else:
+            log.info("Running a single-repository analysis...")
+            result = run_single_repo(module_list_1, algorithm)
 
-        log.info("Beginning full analysis...")
-        clones = run_two_repos(module_list_1, module_list_2, algorithm)
         time_snap("Analysis completed")
 
-        # Create output directory if it doesn't exist and print output
-        output_path = os.getcwd()
-        output_filename = "clones_" + \
+        # Save detection result to a JSON file.
+        json_filename = "clones_" + \
             datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".json"
-        os.makedirs(output_path, exist_ok=True)
-        with open(os.path.join(output_path, output_filename), "w") as output_file:
-            output_file.write(clones.json())
+
+        with open(json_filename, "w") as f:
+            f.write(result.json())
 
     except UserInputError as ex:
         if ex.message:
