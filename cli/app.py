9120d963f8fb31833577efb649c9bb811940094f
Denton Wood <denton_wood@baylor.edu>
2019-11-25 12:14:16 -0600

    changes to file naming

--- cli/app.py
+++ cli/app.py
@@ -42,8 +42,13 @@ def main():
         time_snap("Analysis completed")
 
         # Save detection result to a JSON file.
-        json_filename = "clones_" + \
-            datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".json"
+
+        repo_0 = repos[0][repos[0].rfind('/') + 1:]
+        json_filename = "clones_" + algorithm + "_" + repo_0 + "_"
+        if repos[1]:
+            repo_1 = repos[1][repos[1].rfind('/') + 1:]
+            json_filename += repo_1 + "_"
+        json_filename += datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".json"
 
         with open(json_filename, "w") as f:
             f.write(result.json())
